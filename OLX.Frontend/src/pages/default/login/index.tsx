c73c64fa232ecd374b135361420f631994638bed
dgmnkk <125593353+dgmnkk@users.noreply.github.com>
2025-01-22 20:09:23 +0200

    login page done

--- OLX.Frontend/src/pages/default/login/index.tsx
+++ OLX.Frontend/src/pages/default/login/index.tsx
@@ -1,4 +1,3 @@
-
 import React, { useRef, useState } from 'react'
 import { Button, Checkbox, Divider, Form } from 'antd';
 import { ILoginLocalRequest } from '../../../models/account';
@@ -10,7 +9,6 @@ import { IError, IUserLockoutError } from '../../../models/errors';
 import { useGoogleLoginMutation, useLoginMutation, useSendConfirmEmailMutation } from '../../../redux/api/accountApi';
 import PrimaryButton from '../../../components/primary_button';
 import FormInput from '../../../components/form_input';
-import Title from 'antd/es/typography/Title';
 
 const loginAction: string = 'login'
 
@@ -75,12 +73,10 @@ const LoginPage: React.FC = () => {
     }
   }
 
-
-
   return (
-    <div className="flex h-[100%] w-[100%]">
-      <div className="w-[40%]">
-         <img  src='src\assets\images\login_leftside.png'/>
+    <div className="flex h-screen w-screen">
+      <div className="w-[40%] h-[100%]">
+         <img className="w-[100%] h-[100%]" src='src\assets\images\login_leftside.png'/>
       </div>
       <div id='#login' className="w-[60%] flex flex-col items-center justify-center text-center">
         <h2 className='w-[460px] text-[#3A211C] mb-[50px] font-unbounded text-[36px] font-normal'>З поверненням!</h2>
@@ -98,7 +94,7 @@ const LoginPage: React.FC = () => {
           <FormInput label='Електронна пошта' name='email' placeholder='example@gmail.com' ruleType='email' requiredMessage='Будь ласка, введіть електронну пошту' typeMessage='Неправильно введена пошта' />
           <FormInput label='Пароль' name='password' placeholder='eXampLe_3' requiredMessage='Будь ласка, введіть пароль' />
 
-          <div className='flex justify-between mb-[30px]'>
+          <div className='flex justify-between'>
             <Form.Item
               name="remember"
               valuePropName="checked"
@@ -119,12 +115,9 @@ const LoginPage: React.FC = () => {
           {loginError?.status === 403 &&
             <Button onClick={sendConfirmEmail} color="primary" variant="link">Надіслати лист для підтвердження</Button>
           }
-
         </Form>
-
       </div>
     </div>
-
   )
 }
 export default LoginPage;
