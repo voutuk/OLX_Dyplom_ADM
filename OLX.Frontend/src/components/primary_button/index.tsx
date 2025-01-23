e8844b89a94c0e26f4bbb312535b642ae9a191dc
OlexKov <Kovalchuk.olex@gmail.com>
2025-01-23 20:21:09 +0200

    Fix update

--- OLX.Frontend/src/components/primary_button/index.tsx
+++ OLX.Frontend/src/components/primary_button/index.tsx
@@ -4,19 +4,21 @@ import { Button } from 'antd'
 
 const PrimaryButton: React.FC<PrimaryButtonProps> = ({ onButtonClick = () => { }, title, width = '460px', height = '44px', bgColor = '#FFBA00', fontColor = '#3A211C', isLoading, htmlType = "button" }) => {
   return (
-    <Button
-      loading={isLoading}
-      onClick={onButtonClick}
-      htmlType={htmlType}
-      style={{
-        backgroundColor: bgColor,
-        color: fontColor,
-        width: width,
-        height: height,
-      }}
-      className={`rounded-[8px] text-[20px] font-normal font-montserrat leading-[28px] border-none`}>
-      {title}
-    </Button>
+    <div className="primary-button" >
+      <Button
+        loading={isLoading}
+        onClick={onButtonClick}
+        htmlType={htmlType}
+        style={{
+          backgroundColor: bgColor,
+          color: fontColor,
+          width: width,
+          height: height,
+        }}
+        className={`primary-button rounded-[8px] text-[20px] font-normal font-montserrat leading-[28px] border-none`}>
+        {title}
+      </Button>
+    </div>
   )
 }
 
