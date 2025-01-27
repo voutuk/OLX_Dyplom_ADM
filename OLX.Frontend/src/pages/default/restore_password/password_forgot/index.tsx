5f7eddd0d5c7b56d2eadbdcaaf4b15a305eec10e
OlexKov <Kovalchuk.olex@gmail.com>
2025-01-27 11:04:25 +0200

    Update

--- OLX.Frontend/src/pages/default/restore_password/password_forgot/index.tsx
+++ OLX.Frontend/src/pages/default/restore_password/password_forgot/index.tsx
@@ -1,11 +1,15 @@
-import { Button, Form, Input } from "antd";
+import { Button, Form } from "antd";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { useSendPasswordResetEmailMutation } from "../../../../redux/api/accountApi";
+import PrimaryButton from "../../../../components/primary_button";
+import { Images } from "../../../../constants/images";
+import FormInput from "../../../../components/form_input";
+import { LeftOutlined } from '@ant-design/icons';
 
 const ForgotPasswordPage: React.FC = () => {
-  const [passFogot,{isLoading}] = useSendPasswordResetEmailMutation()
-  const navigator = useNavigate();
+  const [passFogot, { isLoading }] = useSendPasswordResetEmailMutation()
+  const navigate = useNavigate();
 
   const sendPassResetEmail = async (formResult: { email: string }) => {
     const result = await passFogot(formResult.email);
@@ -13,44 +17,51 @@ const ForgotPasswordPage: React.FC = () => {
       toast("Лист відновлененя паролю відправлено на вашу пошту", {
         type: "success"
       })
-      navigator('/')
+      
     }
   }
 
   return (
-    <Form
-      layout='vertical'
-      style={{
-        maxWidth: 300,
-      }}
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={sendPassResetEmail}
-      className='mx-auto text-center'
-      autoComplete="off"
-    >
-      <Form.Item
-        label="Електронна пошта"
-        name="email"
-        rules={[
-          {
-            required: true,
-            message: "Будьласка введіть eлектронну пошту!",
-          },
-          {
-            type: 'email',
-            message: "Невірно введена пошта!",
-          },
-        ]}
-      >
-        <Input type='large' />
-      </Form.Item>
-
-      <Button loading={isLoading} className='mt-3' style={{ width: 200 }} type="primary" htmlType="submit">
-        Надіслати
-      </Button>
-    </Form>
+    <div className="flex h-screen w-screen  justify-between">
+      <div className="w-[50%] h-[100%]">
+        <img className="w-[100%] h-[100%] object-cover" src={Images.loginImage} />
+      </div>
+      <div className="mx-auto my-auto w-[24%] text-center">
+        <div className="flex flex-col gap-2 mb-[8vh] text-center">
+          <span className="font-unbounded text-adaptive-login-header-text ">Забули пароль?</span>
+          <span className="font-montserrat text-adaptive-text">Не хвилюйтесь, ми надішлемо електронний лист з підтвердженням скидання паролю</span>
+        </div>
+        <Form
+          className='w-full'
+          layout='vertical'
+          initialValues={{
+            remember: true
+          }}
+          onFinish={sendPassResetEmail}
+        >
+          <FormInput
+            className='h-[5vh] min-h-[35px] mb-[2vh] text-adaptive-input-form-text text-[#9B7A5B]'
+            label='Електронна пошта'
+            name='email'
+            placeholder='example@gmail.com'
+            rules={[
+              { required: true, message: 'Будь ласка, введіть електронну пошту' },
+              { type: 'email', message: 'Неправильний формат електронної пошти' }
+            ]} />
+          <PrimaryButton
+            htmlType='submit'
+            className='w-full h-[5vh] mb-[8vh]'
+            title='Скинути пароль'
+            isLoading={isLoading} />
+        </Form>
+        <Button onClick={() => navigate(-1)} className='text-[#3A211C] text-adaptive-input-form-error-text shadow-none font-montserrat border-none  ml-[5px]' variant="link">
+          <div className='flex gap-2 items-center'>
+            <LeftOutlined className='text-black text-adaptive-input-form-error-text' />
+            Назад
+          </div>
+        </Button>
+      </div>
+    </div >
   );
 };
 
--- OLX.Frontend/src/redux/middlewares/errorMiddleware.ts
+++ OLX.Frontend/src/redux/middlewares/errorMiddleware.ts
@@ -6,7 +6,18 @@ import { RootState } from '..';
 import { logOut } from '../slices/userSlice';
 
 
-
+const toastBlockedRoutes: string[] = [
+    '/auth/emailconfirm',
+    '/auth/password'
+]
+const showToast = (message: string, type?: TypeOptions, style?: React.CSSProperties) => {
+    if (!toastBlockedRoutes.includes(window.location.pathname)) {
+        toast(message, {
+            type: type,
+            style: style
+        })
+    }
+}
 
 const errorMiddleware: Middleware = (api: MiddlewareAPI) => (next) => (action) => {
     if (isRejectedWithValue(action)) {
