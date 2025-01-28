61c78a6298a10bca93fedcf1dd963227b676fa82
dgmnkk <125593353+dgmnkk@users.noreply.github.com>
2025-01-28 18:01:59 +0200

    initial stylyzing of adverts section ready

--- OLX.Frontend/src/components/adverts_section/index.tsx
+++ OLX.Frontend/src/components/adverts_section/index.tsx
@@ -4,9 +4,9 @@ import { AdvertsSectionProps } from "./props"
 
 const AdvertsSection: React.FC<AdvertsSectionProps> = ({ title, adverts }) => {
   return (
-    <div className="items-center">
-      <h2 className='text-[#3A211C] mb-[6vh] font-unbounded text-adaptive-login-header-text font-normal'>{title}</h2>
-      <div className="flex gap-x-[5px] gap-y-[10px]">
+    <div className="px-[150px] py-[100px]">
+      <h2 className='text-[#3A211C] mb-[6vh] font-unbounded text-adaptive-login-header-text font-normal text-center'>{title}</h2>
+      <div className="grid grid-cols-4 gap-y-[20px]">
         {adverts.map(advert => (
           <AdvertCard key={advert.id} id={advert.id} title={advert.title} image={advert.image} price={advert.price} settlement={advert.settlement} />
         ))}
--- OLX.Frontend/src/pages/default/home/index.tsx
+++ OLX.Frontend/src/pages/default/home/index.tsx
@@ -1,14 +1,73 @@
+import { AdvertCardProps } from '../../../components/advert_card/props';
 import AdvertsSection from '../../../components/adverts_section';
 import HomePageImageBlock from '../../../components/home_page_image';
 import './style.scss'
 
 
 const HomePage: React.FC = () => {
+  const adverts: AdvertCardProps[] = [
+    {
+      id: 1,
+      title: 'Мальтіпу',
+      image: 'src/assets/images/image.png',
+      price: 40000,
+      settlement: 'Київ'
+    },
+    {
+      id: 2,
+      title: 'Мальтіпу',
+      image: 'src/assets/images/image.png',
+      price: 40000,
+      settlement: 'Київ'
+    },
+    {
+      id: 3,
+      title: 'Мальтіпу',
+      image: 'src/assets/images/image.png',
+      price: 40000,
+      settlement: 'Київ'
+    },
+    {
+      id: 4,
+      title: 'Мальтіпу',
+      image: 'src/assets/images/image.png',
+      price: 40000,
+      settlement: 'Київ'
+    },
+    {
+      id: 5,
+      title: 'Мальтіпу',
+      image: 'src/assets/images/image.png',
+      price: 40000,
+      settlement: 'Київ'
+    },
+    {
+      id: 6,
+      title: 'Мальтіпу',
+      image: 'src/assets/images/image.png',
+      price: 40000,
+      settlement: 'Київ'
+    },
+    {
+      id: 7,
+      title: 'Мальтіпу',
+      image: 'src/assets/images/image.png',
+      price: 40000,
+      settlement: 'Київ'
+    },
+    {
+      id: 8,
+      title: 'Мальтіпу',
+      image: 'src/assets/images/image.png',
+      price: 40000,
+      settlement: 'Київ'
+    }
+  ];
 
   return (
     <div className="flex-1 flex flex-col justify-center">
       <HomePageImageBlock />
-      <AdvertsSection title='Рекомендовані оголошення' adverts={[]} />
+      <AdvertsSection title='Рекомендовані оголошення' adverts={adverts} />
     </div>
   );
 };
