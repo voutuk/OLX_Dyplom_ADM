1bf2d7b95e5fed65b43c14eadf51b98a0dcc4a08
OlexKov <Kovalchuk.olex@gmail.com>
2025-02-16 21:00:07 +0200

    Add settings page selector

--- OLX.Frontend/src/pages/user/edit_profile/index.tsx
+++ OLX.Frontend/src/pages/user/edit_profile/index.tsx
@@ -1,16 +1,25 @@
+import { Tabs } from "antd";
+import './style.scss'
+
 const UserProfileEdit: React.FC = () => {
+
+    const items = [
+        { key: "1", label: "Основна інформація", children: <div className="w-full h-[500px] bg-slate-400"> Основна інформація</div> },
+        { key: "2", label: "Історія платежів", children: <div className="w-full h-[500px] bg-slate-400"> Історія платежів</div> },
+        { key: "3", label: "Налаштування конфіденційності", children: <div className="w-full h-[500px] bg-slate-400"> Налаштування конфіденційності</div> },
+    ];
     return (
         <div className="w-[100%] gap-[5vh] mx-[8vw]   flex flex-col my-[22vh]">
             <h1 className="font-unbounded text-adaptive-3_35-text">Мої налаштування</h1>
-            <div className="flex">
-                <div className="w-[44%]  h-[500px] bg-slate-100">
-                    Content
-                </div>
-                <div className="flex-1 h-[500px] bg-slate-200">
-                    Content
-                </div>
-
-            </div>
+            <Tabs
+                className="w-full font-montserrat"
+                defaultActiveKey="1"
+                tabPosition="left"
+                tabBarStyle={{ width: '42%' }}
+                animated={true}
+                tabBarGutter={0}
+                items={items}
+            />
         </div>
     )
 }
