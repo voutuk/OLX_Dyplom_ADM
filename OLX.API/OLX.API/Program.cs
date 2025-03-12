886c8689acbe80209fd20a63e3b0b11a44b7c1d5
Yurii Voitiuk <voutuk@icloud.com>
2025-03-12 05:45:01 +0200

    revert

--- OLX.API/OLX.API/Program.cs
+++ OLX.API/OLX.API/Program.cs
@@ -42,12 +42,3 @@ app.AddCultures();
 app.DataBaseMigrate();
 await app.SeedDataAsync();
 await app.RunAsync();
-
-// FIXME: DELETE ME THIS IS JUST A TEST PIPELINE ERROR
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!")
-    }
-}
